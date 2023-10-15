98771ede48433cb32773c3fa83c15678515d55ce
Jacek Kosinski <kashiash@gmail.com>
2023-10-15 22:33:22 +0200

    Initial Commit

---
+++ ExpenseTrackerX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseTrackerX
+//
+//  Created by Jacek Kosinski U on 15/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenseTrackerX/ExpenseTrackerXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerXApp.swift
+//  ExpenseTrackerX
+//
+//  Created by Jacek Kosinski U on 15/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
