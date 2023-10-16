aaec9c92aebd69d9d6452ce72611fccbdf98d52b
Jacek Kosinski <kashiash@gmail.com>
2023-10-16 19:52:45 +0200

    .

---
+++ ExpenseTrackerX/View/ExpenseCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseCardView.swift
+//  ExpenseTrackerX
+//
+//  Created by Jacek Kosinski U on 16/10/2023.
+//
+
+import SwiftUI
+
+struct ExpenseCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseCardView()
+}
